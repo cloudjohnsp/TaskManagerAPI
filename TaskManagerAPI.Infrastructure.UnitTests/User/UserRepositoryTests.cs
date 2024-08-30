f6fb7a05f1dd058b27ef452d8c059e40a97192ff
johnjoker13 <joaoavelino2008@hotmail.com>
2024-08-30 17:52:48 -0300

    user repository tests

--- TaskManagerAPI.Infrastructure.UnitTests/User/UserRepositoryTests.cs
+++ TaskManagerAPI.Infrastructure.UnitTests/User/UserRepositoryTests.cs
@@ -17,7 +17,7 @@ namespace TaskManagerAPI.Infrastructure.UnitTests;
 
 public class UserRepositoryTests
 {
-     private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IUserRepository> _userRepositoryMock;
     private User _user;
 
     public UserRepositoryTests()
@@ -30,7 +30,6 @@ public class UserRepositoryTests
     public async void Create_Returns_User()
     {
         // Arrange
-        
         _userRepositoryMock.Setup(x => x.Create(It.IsAny<User>()))
             .Returns(Task.FromResult(_user));
         var command = new CreateUserCommand(_user.NickName, _user.Password);
@@ -58,5 +57,64 @@ public class UserRepositoryTests
         result.Should().NotBeNull();
         result.Should().BeOfType<User>();
         result?.NickName.Should().Be("john_doe");
+    }
+
+    [Fact]
+    public async void UpdateNickName_Returns_UpdatedUser()
+    {
+        // Arrange
+        var updatedUser = User.Create("jane_doe", "Password#1090");
+        updatedUser.Id = _user.Id;
+        updatedUser.CreatedAt = _user.CreatedAt;
+        updatedUser.LastUpdatedAt = _user.LastUpdatedAt;
+
+        _userRepositoryMock
+            .Setup(x => x.UpdateNickName(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(Task.FromResult((User?)updatedUser));
+
+        UpdateUserNickNameCommand command = new(_user.Id, updatedUser.NickName);
+        UpdateUserNickNameCommandHandler handler = new(_userRepositoryMock.Object);
+
+        // Act
+        User? result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<User>();
+        result?.NickName.Should().Be("jane_doe");
+    }
+
+    [Fact]
+    public async void Delete_Returns_Nothing()
+    {
+        // Arrange
+        _userRepositoryMock
+            .Setup(x => x.Delete(It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        DeleteUserCommand command = new(_user.Id);
+        DeleteUserCommandHandler handler = new(_userRepositoryMock.Object);
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().Be(Task.CompletedTask);
+    }
+
+    [Fact]
+    public async void UpdatePassword_Returns_Nothing()
+    {
+        // Arrange
+        _userRepositoryMock
+            .Setup(x => x.UpdatePassword(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        UpdateUserPasswordCommand command = new(_user.Id, "Password@01234");
+        UpdateUserPasswordCommandHandler handler = new(_userRepositoryMock.Object);
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().Be(Task.CompletedTask);
     }
 }
