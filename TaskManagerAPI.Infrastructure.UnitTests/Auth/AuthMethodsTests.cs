e0cf604ff2f0cb524228348d249f81bf9bb8b19d
johnjoker13 <joaoavelino2008@hotmail.com>
2024-09-10 19:13:22 -0300

    auth unit testing

--- TaskManagerAPI.Infrastructure.UnitTests/Auth/AuthMethodsTests.cs
+++ TaskManagerAPI.Infrastructure.UnitTests/Auth/AuthMethodsTests.cs
@@ -14,5 +14,30 @@ namespace TaskManagerAPI.Infrastructure.UnitTests;
 
 public class AuthMethodsTests
 {
-    
+    private readonly JwtSettings _jwtSettings;
+    private readonly IOptions<JwtSettings> _optionsMock;
+
+    public AuthMethodsTests()
+    {
+        _jwtSettings = new JwtSettings() 
+        { 
+            Audience = "test",
+            Issuer = "test",
+            ExpiryMinutes = 60,
+            Secret = "kxcKREQtwQgUnLoxtugQQiWIQr9vZy3b"
+        };
+        _optionsMock = Options.Create<JwtSettings>(_jwtSettings);
+    }
+
+    [Fact]
+    public void GenerateToken_Returns_TokenOnSuccess()
+    {
+        // Arrange
+        var user = User.Create("john_doe", "Pass@1234", "Common");
+        JwtGenerator jwtGenerator = new(_optionsMock);
+        // Act
+        var result = jwtGenerator.GenerateToken(user);
+        // Assert
+        result.Should().BeOfType<string>();
+    }
 }
