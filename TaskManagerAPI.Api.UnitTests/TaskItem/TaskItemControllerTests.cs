c112af56249a140606ae55965ed7ddab0e5d8476
johnjoker13 <joaoavelino2008@hotmail.com>
2024-09-11 18:56:59 -0300

    task item controller tests

--- TaskManagerAPI.Api.UnitTests/TaskItem/TaskItemControllerTests.cs
+++ TaskManagerAPI.Api.UnitTests/TaskItem/TaskItemControllerTests.cs
@@ -13,6 +13,7 @@ using TaskManagerAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FluentAssertions;
+using TaskManagerAPI.Application.Queries;
 
 namespace TaskManagerAPI.Api.UnitTests;
 
@@ -40,11 +41,11 @@ public class TaskItemControllerTests
     }
 
     [Fact]
-    public async Task CreateTaskItem_ReturnsStatus201_OnSuccess()
+    public async Task CreateTaskItem_ShouldReturnStatus201Created_WhenSuccessfull()
     {
         // Arrange
         var request = new CreateTaskItemRequest(_taskItem.Description, _taskItem.TaskListId);
-        var command = new CreateTaskItemCommand(_taskItem.Description, _taskItem.TaskListId);
+        var command = new CreateTaskItemCommand(request.Description, request.TaskListId);
 
         _mapperMock.Setup(m => m.Map<CreateTaskItemCommand>(request))
             .Returns(command);
@@ -66,4 +67,105 @@ public class TaskItemControllerTests
         _mediatorMock.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
         _mapperMock.Verify(m => m.Map<TaskItemResponse>(_taskItem), Times.Once);
     }
+
+    [Fact]
+    public async Task GetTaskItem_ShouldReturn200OK_WhenTaskItemIsFound()
+    {
+        // Arrange
+        var query = new GetTaskItemQuery(_taskItem.Id);
+
+        _mediatorMock.Setup(m => m.Send(query, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_taskItem);
+        _mapperMock.Setup(m => m.Map<TaskItemResponse>(_taskItem))
+            .Returns(_taskItemResponse);
+
+        // Act
+        var result = await _taskItemController.GetTaskItem(query.Id);
+
+        // Assert
+        var actionResult = result.Result as ObjectResult;
+        actionResult.Should().NotBeNull();
+        actionResult!.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+        var resultValue = actionResult.Value as TaskItemResponse;
+        resultValue.Should().BeEquivalentTo(_taskItemResponse);
+
+        _mediatorMock.Verify(m => m.Send(query, It.IsAny<CancellationToken>()), Times.Once);
+        _mapperMock.Verify(m => m.Map<TaskItemResponse>(_taskItem), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateTaskItem_ShouldReturn200OK_WhenTaskItemIsUpdated()
+    {
+        // Arrange
+        var request = new UpdateTaskItemRequest(_taskItem.Id, "New Description");
+        var command = new UpdateTaskItemCommand(request.Id, request.Description);
+
+        _mediatorMock.Setup(m => m.Send(command, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_taskItem);
+        _mapperMock.Setup(m => m.Map<TaskItemResponse>(_taskItem))
+            .Returns(_taskItemResponse);
+
+        // Act
+        var result = await _taskItemController.UpdateTaskItem(request);
+
+        // Assert
+        var actionResult = result.Result as ObjectResult;
+        actionResult.Should().NotBeNull();
+        actionResult!.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+        var resultValue = actionResult.Value as TaskItemResponse;
+        resultValue.Should().BeEquivalentTo(_taskItemResponse);
+
+        _mediatorMock.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
+        _mapperMock.Verify(m => m.Map<TaskItemResponse>(_taskItem), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateTaskItemStatus_ShouldReturn200OK_WhenTaskItemStatusIsUpdated()
+    {
+        // Arrange
+        var request = new UpdateTaskItemStatusRequest(_taskItem.Id, true);
+        var command = new UpdateTaskItemStatusCommand(request.Id, request.IsDone);
+
+        _mediatorMock.Setup(m => m.Send(command, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_taskItem);
+        _mapperMock.Setup(m => m.Map<TaskItemResponse>(_taskItem))
+            .Returns(_taskItemResponse);
+
+        // Act
+        var result = await _taskItemController.UpdateTaskItemStatus(request);
+
+        // Assert
+        var actionResult = result.Result as ObjectResult;
+        actionResult.Should().NotBeNull();
+        actionResult!.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+        var resultValue = actionResult.Value as TaskItemResponse;
+        resultValue.Should().BeEquivalentTo(_taskItemResponse);
+
+        _mediatorMock.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
+        _mapperMock.Verify(m => m.Map<TaskItemResponse>(_taskItem), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteTaskItem_ShouldReturn204NoContent_WhenTaskItemIsDeleted()
+    {
+        // Arrange
+        var command = new DeleteTaskItemCommand(_taskItem.Id);
+
+        _mediatorMock.Setup(m => m.Send(command, It.IsAny<CancellationToken>()))
+            .Verifiable();
+
+        // Act
+        var result = await _taskItemController.DeleteTaskItem(command.Id);
+
+        // Assert
+        var statusResult = result as StatusCodeResult;
+        statusResult.Should().NotBeNull();
+        statusResult!.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+
+        _mediatorMock.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
 }
